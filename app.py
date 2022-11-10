ea81c4d4e004ebff0d641f060a643a1ac022aee9
Chy Plutchak <102931751+cplutchak@users.noreply.github.com>
2022-11-09 17:48:27 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,9 @@
 from flask import Flask, render_template, json, redirect, request
-from flask_modals import Modal, render_template_modal
 from flask_bootstrap import Bootstrap4
 import os
-import static.database.db_connector as db
 
 app = Flask(__name__)
-modal = Modal(app)
 
-db_connection = db.connect_to_database()
 
 bootstrap = Bootstrap4(app)
 
@@ -15,86 +11,9 @@ bootstrap = Bootstrap4(app)
 def homepage():  
     return render_template('index.html')
 
-# Gym Read and Create
-@app.route('/gyms', methods=["POST", "GET"])
+@app.route('/gyms')
 def gyms_page():
-    # Contains post request method for adding gym.
-    if request.method == "POST":
-        if request.form.get("Add_Gym"):
-            gym_nameInput = request.form["gym_name"]
-            gym_addressInput = request.form["gym_address"]
-            gym_zipInput = request.form["gym_zip"]
-            gym_cityInput = request.form["gym_city"]
-            gym_stateInput = request.form["gym_state"]
-
-            for _ in [gym_addressInput, gym_zipInput, gym_cityInput, gym_stateInput]:
-                if _ == "":
-                    _ = "NULL"
-
-            query = f"INSERT INTO gyms (gym_name, gym_address, gym_zip, gym_city, gym_state) \
-                    VALUES (\"{gym_nameInput}\", \"{gym_addressInput}\", {gym_zipInput}, \"{gym_cityInput}\", \"{gym_stateInput}\");"
-            print(query)
-            cursor = db.execute_query(db_connection=db_connection, query=query)
-            return redirect('/gyms')
-
-            # Only gym_name is non-nullable. 
-            # Conditions below to handle differnt sets of inputs.
-            if gym_addressInput == gym_zipInput == gym_cityInput ==gym_stateInput == "":
-                query = f"INSERT INTO gyms (gym_name) VALUES (\"{gym_nameInput}\");"
-                print(query)
-                cursor = db.execute_query(db_connection=db_connection, query=query)
-                return redirect('/gyms')
-                
-    if request.method == "GET":
-        # SQL query and execution to populate table on gyms.html
-        query = "SELECT gym_id, gym_name, gym_address, gym_zip, gym_city, gym_state, COUNT(trainers.trainer_id) AS members FROM gyms\
-        LEFT JOIN trainers ON trainers.gyms_gym_id = gyms.gym_id\
-        GROUP BY gym_id\
-        ORDER BY gym_name;"
-        cursor = db.execute_query(db_connection=db_connection, query=query)
-        results = cursor.fetchall()
-        return render_template('gyms.html', gyms=results)
-
-# Gym Deletion
-@app.route('/delete_gym/<int:id>')
-def delete_gym(id):
-    # SQL query and execution to delete gym by passed id
-    query = f"DELETE FROM gyms WHERE gym_id = \"{id}\""
-    cursor = db.execute_query(db_connection=db_connection, query=query)
-    return redirect('/gyms')
-
-# Gym Update
-@app.route('/update_gym/<int:id>', methods=["POST", "GET"])
-def update_gym(id):
-    if request.method == "GET":
-        # SQL query to grab the info of the gym with our passed id
-        query = f"SELECT * FROM gyms WHERE gym_id = \"{id}\""
-        cursor = db.execute_query(db_connection=db_connection, query=query)
-        results = cursor.fetchall()   
-        
-        return render_template_modal('/gyms', modal ='updateGym') 
-
-    if request.method == "POST":
-        if request.form.get("Update_Gym"):
-            gym_nameInput = request.form["gym_name"]
-            gym_addressInput = request.form["gym_address"]
-            gym_zipInput = request.form["gym_zip"]
-            gym_cityInput = request.form["gym_city"]
-            gym_stateInput = request.form["gym_state"]
-
-            # Only gym_name is non-nullable. 
-            # Conditions below to handle differnt sets of inputs.
-            if gym_addressInput == gym_zipInput == gym_cityInput ==gym_stateInput == "":
-                query = f"UPDATE gyms SET gym.gym_name = \"{gym_nameInput}\", gyms.gym_address = NULL, gyms.gym_zip = NULL, gyms.gym_city = NULL, gyms.gym_state = NULL;"
-                cursor = db.execute_query(db_connection=db_connection, query=query)
-                db_connection.commit()
-                return redirect('/gyms')
-
-            else:
-                query = f"UPDATE gyms SET gym.gym_name = \"{gym_nameInput}\", gyms.gym_address = \"{gym_addressInput}\", gyms.gym_zip = \"{gym_zipInput}\", gyms.gym_city = \"{gym_cityInput}\", gyms.gym_state = \"{gym_stateInput}\";"
-                cursor = db.execute_query(db_connection=db_connection, query=query)
-                db_connection.commit()
-                return redirect('/gyms')
+    return render_template("gyms.html")
 
 @app.route('/trainers')
 def trainer_page():
